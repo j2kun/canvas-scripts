93ee2e762798ab73b2af7bdef0587ce683d5646b
Daniel Nghiem <nghiem.daniel@yahoo.com>
2020-04-24 21:56:31 -0700

    Add code to perform a simple upload of a google sheet

--- quickstart.py
+++ quickstart.py
@@ -5,8 +5,10 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+from apiclient.http import MediaFileUpload
+
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
     """Shows basic usage of the Drive v3 API.
@@ -33,17 +35,18 @@ def main():
 
     service = build('drive', 'v3', credentials=creds)
 
-    # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-
+    # Upload  the csv file 
+    file_metadata = {
+        'name': 'gradebook',
+        'mimeType': 'application/vnd.google-apps.spreadsheet'
+    }
+    media = MediaFileUpload('gradebook_2020-04-24.csv',
+                            mimetype='text/csv'
+                            )
+    file = service.files().create(body=file_metadata,
+                                  media_body=media,
+                                  fields='id').execute()
+    print('Your gradebook was uploaded!')
+  
 if __name__ == '__main__':
     main()
